97fa9a52f55039bc0a8e3f6771f353cf3e5818f7
raunak123455 <91775838+raunak123455@users.noreply.github.com>
2025-01-02 14:04:18 +0530

    Update Dashboard.jsx

--- frontend/vite-project/src/components/Dashboard.jsx
+++ frontend/vite-project/src/components/Dashboard.jsx
@@ -13,62 +13,11 @@ import Analytics from "./Analytics";
 import Settings from "./Settings";
 import SharePopup from "./SharePopup";
 import { useNavigate } from "react-router-dom";
+import Modal from "./Modal";
+
 
 // Modal component
-const Modal = ({ isOpen, onClose, onAdd }) => {
-  const [email, setEmail] = useState("");
-  const [Adding, setAdding] = useState(true);
 
-  if (!isOpen) return null;
-
-  const handleAdd = () => {
-    onAdd(email);
-
-    // setEmail(""); // Clear the input field after adding
-    setAdding(false); // Close the modal after adding
-  };
-
-  const handleClose = () => {
-    setEmail("");
-    setAdding(true);
-    onClose();
-  };
-
-  return (
-    <div className={styles.modalOverlay}>
-      <div className={styles.modalContent}>
-        {Adding ? (
-          <>
-            {" "}
-            <h3>Add people to the board</h3>
-            <input
-              type="email"
-              placeholder="Enter the email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <div className={styles.modalActions}>
-              <button onClick={onClose} className={styles.cancelButton}>
-                Cancel
-              </button>
-              <button onClick={handleAdd} className={styles.addButton}>
-                Add Email
-              </button>
-            </div>{" "}
-          </>
-        ) : (
-          <div>
-            <h2 className={styles.added}> {email} has been added}</h2>
-
-            <button onClick={handleClose} className={styles.addButton}>
-              Ok Got it
-            </button>
-          </div>
-        )}
-      </div>
-    </div>
-  );
-};
 
 const Dashboard = () => {
   const { user, loggedIn, setLoggedIn } = useUser(); // Get user and loggedIn state
